89d1f25fef93b63cda036f4218b704f9fc74bf34
Dipesh Aggarwal <1311129+DipeshAggarwal@users.noreply.github.com>
2020-05-06 19:24:25 +0530

    Change wording

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var toData = {};
 
 $('.ui.dropdown')
   .dropdown({
-    placeholder:'Choose State',
+    placeholder:'State',
     values: function () {
       var stateList = ['Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chandigarh','Chhattisgarh','Delhi','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu and Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Pudicherry','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttarakhand','Uttar Pradesh','West Bengal','Andaman and Nicobar Islands','Dadra and Nagar Haveli','Daman and Diu','Lakshadweep'];
       var _returnObj = [];
